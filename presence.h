6342d290550bd4bad403efca0ab22d4b40c771ec
1nekomata <sp.pflaumer@gmail.com>
2022-04-24 19:03:56 +0200

    removed old files

--- presence.h
+++
@@ -1,7 +0,0 @@
-#ifndef DISCORD_H
-#include <orca/discord.h>
-#endif
-
-char *activity_msg[3];// = { "Trying out new Features...", "i hate bugfixing -_-", "idle" };
-enum discord_activity_types activity_type = DISCORD_ACTIVITY_GAME;
-bool afk = false;
