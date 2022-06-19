0770d4a12c10a3fab327c815dda58e15b1131a80
1nekomata <73189703+1nekomata@users.noreply.github.com>
2022-06-19 12:54:49 +0200

    Add Copyright Notice to main.c
    applies to *ALL* files inside src/

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+/*
+ * PLEASE READ THE COPYRIGHT NOTICE AT THE END OF THIS FILE!
+/*
+
 #include <assert.h>
 #include <bits/getopt_core.h>
 #include <stdio.h>
@@ -95,3 +99,30 @@ int main(int argc, char *argv[]){
 
     return 0;
 }
+
+// Copytight Notice of the MIT License
+
+/* The following Notice applies to *ALL* files inside the src/ folder of this Project
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2022 1nekomata and patemathic
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
